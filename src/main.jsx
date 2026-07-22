9a9b7f4167dda0678c288ed20a37eca17cb9bfb2
dewanxt <sabbirdewann@gmail.com>
2026-07-23 00:47:48 +0600

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import firebaseConfig from './firebaseConfig.js'
-import ContextApi from './ContextApi.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ContextApi />
+    <App/>
   </StrictMode>,
 )
--- src/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import { counterSlice } from './slices/CounterSlice'
-
-export default configureStore({
-  reducer: {
-    Counter : counterSlice.reducer
-  },
-})
